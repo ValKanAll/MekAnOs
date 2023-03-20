a267959004d28fe1683ba3d891e4d841030b0a71
VAL <valentin.allard@rocketmail.com>
2023-03-20 15:03:36 +0100

    Requirements fixes

---
+++ Global_paths.py
@@ -0,0 +1,10 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+print('ROOT_DIR = ', ROOT_DIR)
+
+dataset_path = os.path.join(ROOT_DIR, 'MekAnos', 'Data', 'Samples.xlsx')
+
+literature_laws_path = os.path.join(ROOT_DIR, 'MekAnos', 'Data', 'Litterature_laws.mkbl')
+
+ansysWB_path = r"C:\Program Files\ANSYS Inc\v211\Framework\bin\Win64\RunWB2.exe"
